a35fb839f021f55969bbb03111e80d9992e0972e
Padmini Jaikumar <padmini.jaikumar@gmail.com>
2016-03-31 23:33:07 -0700

    Fixed IRR formula

--- rental_roi_calc.py
+++ rental_roi_calc.py
@@ -8,12 +8,12 @@ import numpy
 house_cost=240000 #total cost: land + house
 depreciable_value=0.7 #% of house_cost that is the house
 monthly_rent=1400
-analysis_for_n_years=20
+analysis_for_n_years=10
 rent_increase_rate=0.04
 house_appreciation_rate=0.04
 mortgage_rate=0.042
 mortgage_duration=15
-down_payment=0.20
+down_payment=0.25
 
 
 ##Other rates that impact calculation. Need not be changed.
@@ -23,7 +23,7 @@ prop_management_rate = 0.1 #% of rent
 yearly_maintenance=2000
 months_vacant=1
 closing_fees=0.00
-selling_costs=0.06
+selling_costs=0.04 #% of house cost
 yearly_trip=250
 one_time_fixup_before_selling=20000
 hoa_per_month=40
@@ -32,7 +32,7 @@ hoa_per_month=40
 
 ### Grand Totals
 net_earnings_over_analysis_years_from_rent_after_tax=0
-total_money_spent_on_house=down_payment*house_cost
+total_money_spent_on_house=0
 total_depreciation_claimed=0
 
 loan_principal_balance=(1-down_payment)*house_cost
@@ -112,7 +112,7 @@ for year in range(1, analysis_for_n_years+1):
         net_cash_flow-=down_payment*house_cost
     net_cash_flow_arr.append(net_cash_flow)
     tax_impact=cash_in-tax_deductible_expenses
-    print "Year " + str(year) + "-> Net Cash Flow:" + str(net_cash_flow) + ", Mortgage payments: " + str(yearly_mortgage_payment)
+    print "Year " + str(year) + "-> Net Cash Flow:" + str(net_cash_flow)
 
     ### Add to net earning over analysis years
     #If tax impact > 0, we made money. Add to net_earnings after deducting 33% tax
@@ -136,13 +136,13 @@ roi = (total_earnings_after_n_years - total_money_spent_on_house)*100/(total_mon
 ########## IRR #################################
 net_cash_flow_arr.append(gain_from_selling_house_after_tax)
 final_irr = numpy.irr(net_cash_flow_arr)*100
-print "IRR: " + str(final_irr)
+print "After Tax IRR: " + str(final_irr)
 
 
 print "Earnings after " + str(analysis_for_n_years) + " years -> " + "Total: " + str(total_earnings_after_n_years) + \
     ", From yearly rent - expenses: " + str(net_earnings_over_analysis_years_from_rent_after_tax) + ", Selling house: " + str(gain_from_selling_house_after_tax)
 print "Total Money spent on house: " + str(total_money_spent_on_house)
-print "Net ROI per year (after tax): " + str(roi)
+print "Simple ROI per year (after tax): " + str(roi)
 
 
 
